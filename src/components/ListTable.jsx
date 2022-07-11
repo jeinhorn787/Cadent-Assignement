f432177f2cee4cba3d3e7a7e988c1d3f4bc0f40a
Jeremy Einhorn <jeremyeinhorn@Jeremys-MacBook-Pro.local>
2022-07-10 20:49:35 -0400

    small cleanup

--- src/components/ListTable.jsx
+++ src/components/ListTable.jsx
@@ -6,7 +6,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { Button } from '@mui/material';
+import Button from '@mui/material/Button';
 
 
 export const ListTable = () => {
@@ -27,7 +27,7 @@ export const ListTable = () => {
             <TableCell align="right">Name</TableCell>
             <TableCell align="right">Category</TableCell>
             <TableCell align="right">Delivery Method</TableCell>
-            <TableCell align="right">Actions</TableCell>
+            <TableCell align="center">Actions</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -2,5 +2,5 @@ import rootReducer from '../reducers';
 import {createStore} from 'redux';
 
 export default (initialState) => {
-  return createStore(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  return createStore(rootReducer, initialState);
 };
